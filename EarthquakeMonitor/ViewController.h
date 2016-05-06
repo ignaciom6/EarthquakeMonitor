b31e94330749ae6196734888e6ffa5fb9e2740d2
Ignacio Mariani <ignaciom6@hotmail.com>
2016-05-06 18:10:11 -0300

    Initial Commit

---
+++ EarthquakeMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EarthquakeMonitor
+//
+//  Created by Ignacio on 6/5/16.
+//  Copyright © 2016 Ignacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
